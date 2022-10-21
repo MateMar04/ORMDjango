95345014916abe8289fe74b2c2b80a554d97ba92
Mateo <mateo.marchisone@gmail.com>
2022-10-21 08:48:58 -0300

    Hasta ejercicio 8

--- ejercicios.py
+++ ejercicios.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from confApp.models import *
 
 # EJERCICIO 1
@@ -26,4 +28,24 @@ for planta in plantas:
 
 # EJERCICIO 5
 
+localidades = Localidades.objects.all().order_by('nombre')
+for localidad in localidades:
+    print(localidad.nombre)
 
+# EJERCICIO 6
+
+clientes = Clientes.objects.all()
+
+# EJERCICIO 7
+
+plantas = Plantas.objects.all()
+for planta in plantas:
+    precio_off = (planta.precio - (planta.precio * Decimal(0.10))).__round__(2)
+    print(
+        f"CODIGO PLANTA: {planta.cod_planta}, TIPO DE PLANTA: {planta.cod_tipo_planta} DESCRIPCION: {planta.descripcion}, PRECIO: {planta.precio}, OFERTA: {precio_off}, STOCK: {planta.stock}")
+
+# EJERCICIO 8
+plantas = Plantas.objects.all()
+for planta in plantas:
+    precio_total = (Decimal(planta.precio) * planta.precio).__round__(2)
+    print(f"{planta.descripcion}, PRECIO: {planta.precio}, STOCK: {planta.stock} PRECIO TOTAL: {precio_total}")
